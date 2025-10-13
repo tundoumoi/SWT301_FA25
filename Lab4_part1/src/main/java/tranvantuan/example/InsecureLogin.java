dacc04ab918c322507606132c9ad75d27fd58666
tundoumoi <vant7125@gmail.com>
2025-10-13 16:41:50 +0700

    Lab4_Part1

---
+++ Lab4_part1/src/main/java/tranvantuan/example/InsecureLogin.java
@@ -0,0 +1,23 @@
+package tranvantuan.example;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class InsecureLogin {
+
+    private static final Logger logger = Logger.getLogger(InsecureLogin.class.getName());
+
+    public static void login(String username, String password) {
+        if (username.equals("admin") && password.equals(System.getenv("ADMIN_PASSWORD"))) {
+            logger.info("Login successful");
+        } else {
+            logger.warning("Login failed");
+        }
+    }
+
+    public void printUserInfo(String user) {
+        if (user != null && !user.isEmpty()) {
+            logger.log(Level.INFO, "User: {0}", user);
+        }
+    }
+}
+
